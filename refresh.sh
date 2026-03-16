d5a53685a404030867e17e593ef2d899a403d545
Alex <alex0123komyshka@gmail.com>
2026-03-16 20:28:23 +0100

    add refresh script and cron job for daily data pipeline

---
+++ refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 /Users/alexkomyshnyi/Desktop/premier-league-analytics/ingestion/fetch_matches.py
+
+cd /Users/alexkomyshnyi/Desktop/premier-league-analytics/dbt/pl_analytics
+dbt build
